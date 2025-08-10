d90cb500750942cb8b1b10a667cd6df2b403c9aa
AsifurRahman10 <1720105@iub.edu.bd>
2025-08-10 15:52:52 +0600

    user register controller added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,8 +1,47 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadImageOnCloud } from "../utils/fileUploader.js";
 
 
-export const registerUser = asyncHandler((req, res) => {
-    res.status(201).json({
-        message: "Registration successful"
+export const registerUser = asyncHandler(async (req, res) => {
+    const { userName, email, fullName, password } = req.body;
+    if ([userName, email, fullName, password].some(field => field.trim() === '')) {
+        throw new ApiError(400, 'All field is required')
+    }
+    const userAlreadyExist = await User.findOne({
+        $or: [{ userName }, { email }]
     })
+    if (userAlreadyExist) {
+        throw new ApiError(409, 'User already exist')
+    }
+
+    const avatarLocalPath = req.files?.avatar[0].path;
+    const coverLocalPath = req.files?.coverImage[0]?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, 'Avatar is required')
+    }
+
+    const uploadAvatar = await uploadImageOnCloud(avatarLocalPath)
+    const uploadCoverImage = await uploadImageOnCloud(coverLocalPath)
+
+    if (!uploadAvatar.url) {
+        throw new ApiError(400, 'Server error')
+    }
+
+    const user = await User.create(
+        {
+            userName: userName.toLowerCase(), email: email.toLowerCase(), password, avatar: uploadAvatar.url, coverImage: uploadCoverImage.url || '',
+        }
+    )
+    console.log(user);
+
+    const createdUser = await User.findById(user._id).select('-password -refreshToken')
+
+    if (!createdUser) {
+        throw new ApiError(500, 'Something went wrong')
+    }
+    res.status(201).json(new ApiResponse(201, createdUser, 'User created successfully'))
 })
