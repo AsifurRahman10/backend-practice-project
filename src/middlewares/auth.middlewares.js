f3bdf5415c1a5df37c087bbe2484ff5aa712181a
AsifurRahman10 <1720105@iub.edu.bd>
2025-08-16 18:51:03 +0600

    add : verify token middleware added

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,23 @@
+import { User } from "../models/user.models";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from 'jsonwebtoken'
+
+
+const verifyJwt = asyncHandler(async (req, res, next) => {
+    const token = res.cookies.accessToken || req.header("Authorization")?.replace('Bearer ', '');
+    if (!token) {
+        throw new ApiError(400, "Unauthorized access")
+    }
+
+    const decode = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decode?._id).select("-password -refreshToken")
+
+    if (!user) {
+        throw new ApiError(400, "Unauthorized access")
+    }
+
+    req.user = user;
+    next()
+})
