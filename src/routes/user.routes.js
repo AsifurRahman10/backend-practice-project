fcc599b2371488932d6c9b32f0bafb9f9be54d7c
AsifurRahman10 <1720105@iub.edu.bd>
2025-08-24 18:22:16 +0600

    add : define routes for the controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
+import { changePassword, getCurrentUser, getUserProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAvatar, updateCoverImage, updateUserInfo } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
 
@@ -18,8 +18,16 @@ router.route('/register').post(
     ])
     , registerUser)
 router.route('/login').post(loginUser)
-router.route('/refresh-token').post(refreshAccessToken)
+
 
 // secured route
 router.route('/logout').post(verifyJwt, logoutUser)
+router.route('/refresh-token').post(refreshAccessToken)
+router.route('/change-password').post(verifyJwt, changePassword)
+router.route('/current-user').get(verifyJwt, getCurrentUser)
+router.route('/update-user').patch(verifyJwt, updateUserInfo)
+router.route('/avatar').upload.single('avatar').patch(verifyJwt, updateAvatar)
+router.route('/cover-image').upload.single('/coverImage').patch(verifyJwt, updateCoverImage)
+router.route('/profile/:username').get(verifyJwt, getUserProfile)
+router.route('/watch-history').get(verifyJwt, getWatchHistory)
 export default router 
